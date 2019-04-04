a1178e10db3c2ca2af0675fd1c1ca6489a59900b
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2019-04-04 18:15:41 +0700

    Initial Commit

---
+++ InitialProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InitialProject
+//
+//  Created by Bill Tanthowi Jauhari on 04/04/19.
+//  Copyright © 2019 Bill Tanthowi Jauhari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
